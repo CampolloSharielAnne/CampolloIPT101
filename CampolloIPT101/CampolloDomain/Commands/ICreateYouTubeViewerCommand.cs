d4cc40a5b6185e66297dabf374a6a3c26375e92b
CampolloSharielAnne <143675286+CampolloSharielAnne@users.noreply.github.com>
2025-11-20 13:37:51 +0800

    Add files via upload

---
+++ CampolloIPT101/CampolloDomain/Commands/ICreateYouTubeViewerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Campollo.CampolloDomain.Models;
+
+namespace Campollo.CampolloDomain.Commands
+{
+    public interface ICreateYouTubeViewerCommand
+    {
+        Task Execute(YouTubeViewer youTubeViewer);
+    }
+}
---
+++ CampolloIPT101/CampolloDomain/Commands/IUpdateYouTubeViewerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Campollo.CampolloDomain.Models;
+
+namespace Campollo.CampolloDomain.Commands
+{
+    public interface IUpdateYouTubeViewerCommand
+    {
+        Task Execute(YouTubeViewer youTubeViewer);
+    }
+}
