2b2111bc892153a2ef691a41bde9f3a2fa0f9cda
CampolloSharielAnne <143675286+CampolloSharielAnne@users.noreply.github.com>
2025-11-20 13:44:36 +0800

    Add files via upload

---
+++ CampolloIPT101/CampolloYouTubeViewer/ViewModels/EditYouTubeViewerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Campollo.CampolloYouTubeViewer.Commands;
+using Campollo.CampolloDomain.Models;
+using Campollo.CampolloYouTubeViewer.Stores;
+
+namespace Campollo.CampolloYouTubeViewer.ViewModels
+{
+    public class EditYouTubeViewerViewModel : ViewModelBase
+    {
+        public Guid YouTubeViewerId { get; }
+
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public EditYouTubeViewerViewModel(YouTubeViewer youTubeViewer, YouTubeViewersStore youTubeViewersStore, ModalNavigationStore modalNavigationStore)
+        {
+            YouTubeViewerId = youTubeViewer.Id;
+
+            ICommand submitCommand = new EditYouTubeViewerCommand(this, youTubeViewersStore, modalNavigationStore);
+            ICommand cancelCommand = new CloseModalCommand(modalNavigationStore);
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel(submitCommand, cancelCommand)
+            { 
+                Username = youTubeViewer.Username,
+                IsSubscribed = youTubeViewer.IsSubscribed,
+                IsMember = youTubeViewer.IsMember,
+            };
+        }
+    }
+}
